f47ae0f5e7dccbfcf2baecec532454debc8a2f80
vladtsk <vlad.tsk150@gmail.com>
2025-10-02 11:18:09 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <PageHeader />
+        <SideMenu />
+        <AssignmentsSection />
+      </div>
+    </>
+  );
+}
+
+function PageHeader() {
+  return <div>CheckInTracker</div>;
+}
+
+function SideMenu() {
+  return <div></div>;
+}
+
+function AssignmentsSection() {
+  return <div></div>;
+}
+
+export default App;
